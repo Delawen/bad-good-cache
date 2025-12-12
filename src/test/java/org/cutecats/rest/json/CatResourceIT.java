225393e2428fa6f552a2efd98f16167558dcd9e2
María Arias de Reyna Domínguez <mariasde@ibm.com>
2025-12-12 12:34:29 +0100

    comments commenting commentaries

--- src/test/java/org/cutecats/rest/json/CatResourceIT.java
+++ src/test/java/org/cutecats/rest/json/CatResourceIT.java
@@ -10,17 +10,22 @@ import static io.restassured.RestAssured.given;
 @QuarkusIntegrationTest
 class CatResourceIT {
 
-	// Running just once will not be enough
+	// This should help profiling the method
+	// (although real life requests are not always the same,
+	// don't test the happy path only)
 	@RepeatedTest(10000)
 	void newRandomCat() {
 		given()
 				//This must be like production, not a test
+				//This is an important difference with unit tests!
 				.when().get("/cats?test=false")
 				.then()
 				.statusCode(200);
 	}
 
 
+	//This will make sure metadata gets loaded
+	//It will not profile the /list entrypoint method
 	@Test
 	void list() {
 		given()
